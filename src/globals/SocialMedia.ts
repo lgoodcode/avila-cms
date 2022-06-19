06081e35f5b12a5a550b07d102bff9b2fb366b87
Lawrence Good <lawrence.good55@gmail.com>
2022-06-19 15:30:52 -0700

    v1.1.1 Added LastModifiedBy field to Globals
    Added the LastModifiedBy field to all Globals so in addition to the
    timestamp at the bottom, you can identify the user that made the change.
    
    Updated types to reflect the changes.

--- src/globals/SocialMedia.ts
+++ src/globals/SocialMedia.ts
@@ -1,12 +1,14 @@
 import { GlobalConfig } from '../types/payload'
 import { defaultGlobalAccess } from '../lib/accessControl'
 import Link from '../fields/Link'
+import LastModifiedBy from '../fields/LastModifiedBy'
 
 const SocialMedia: GlobalConfig = {
 	slug: 'social-media',
 	label: 'Social Media',
 	access: defaultGlobalAccess,
 	fields: [
+		LastModifiedBy,
 		{
 			name: 'links',
 			type: 'array',
